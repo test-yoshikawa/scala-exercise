fe2bf3b327fb0342b2fced62548eaa1d84c9b0d7
Yuki Yoshikawa <Yuki Yoshikawa@YUKI-YOSHIKAWA>
2015-09-24 18:30:11 +0900

    Chapter10 サンプル

---
+++ src/scalableProgramming/chapter10/UniformElement.scala
@@ -0,0 +1,8 @@
+package scalableProgramming.chapter10
+
+/**
+ * @author Yuki Yoshikawa
+ */
+class UniformElement(ch: Char, override val width: Int, override val height: Int) extends ElementOld {
+	def contents = Array.fill(height)(ch.toString * width)
+}
