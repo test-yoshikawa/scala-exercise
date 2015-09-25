05435a2b80cda1f0b10a0dc72f72f632e385a9c6
Yuki Yoshikawa <Yuki Yoshikawa@YUKI-YOSHIKAWA>
2015-09-25 17:08:53 +0900

    Chapter14 サンプル

---
+++ src/scalableProgramming/chapter13/main.scala
@@ -0,0 +1,24 @@
+package scalableProgramming.chapter13
+
+/**
+ * @author Yuki Yoshikawa
+ */
+object main {
+
+	def main(args: Array[String]) {
+		//========================================
+		// インポート　サンプル
+		//========================================
+		import Fruits.{_}				// Fruitsのすべてのメンバーをインポートする
+		import Fruits.{Orange, Grape}	// インポートするFruitsのメンバーを指定する
+		import Fruits.{Apple => _, _}	// Appleを除くFruitsのすべてのメンバーをインポートする
+
+		// val apple = Apple	// オブジェクトのインポートをしていないためコンパイルエラー
+		val orange = Orange
+		val Pear = Grape
+
+		// メンバーの一部の名前を変更できる
+		import Fruits.{Apple => McIntosh}
+		val apple = McIntosh
+	}
+}
