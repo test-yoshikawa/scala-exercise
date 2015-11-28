85ef12d148e247d26cdf42df5e618aa1ea72af6f
yuki-yoshikawa <passion.efforts.freelance@gmail.com>
2015-11-29 00:24:31 +0900

    サンプル追加

--- src/main/scala/12_Collections/mapSample.scala
+++ src/main/scala/12_Collections/mapSample.scala
@@ -1,25 +1,33 @@
-package scala.Collections
+package Collections
 
 import scala.collection._
 
 /**
- * @author Yuki Yoshikawa
+ * Map　サンプル
  */
-object mapSample {
+object MapSample {
 
 	def main(args: Array[String]) {
 		val map = mutable.Map.empty[String, Int]
 		map("hello") = 1
 		map("there") = 2
-		println("map変数：" + map)
-		println("map(\"hello\")：" + map("hello"))
-		println("map(\"there\")：" + map("there"))
-		// println("map(\"empty\")：" + map("empty"))	// 例外発生：NoSuchElementException
+		println("map variable: " + map)
+		println("map(\"hello\") value: " + map("hello"))
+		println("map(\"there\") value: " + map("there"))
+		try {
+			println("map(\"empty\")：" + map("empty"))	// 例外発生：NoSuchElementException
+		} catch {
+			case e: NoSuchElementException => println("NoSuchElementException")
+		}		
 
 		// 文章から単語を取得する
 		val text = "See Spot run. Run, Spot . Run!"
-		print("Mapを使用して単語を取得：")
+		print("get words by Map : ")
 		println(countWords(text))
+
+		println("\n==== Map methods ====")
+		showMapMethods()
+
 	}
 
 	/**
@@ -36,5 +44,34 @@ object mapSample {
 			counts += (word -> (oldCount + 1))
 		}
 		counts
+	}
+
+	/**
+	 * Map の一般的な操作を出力する
+	 */
+	def showMapMethods() {
+		val nums = Map("i" -> 1, "li" -> 2)	// immutable な Map を作成
+		println("nums + (\"vi\" -> 6): " + (nums + ("vi" -> 6)))	// エントリーを追加する
+		println("nums - \"li\": " + (nums - "li")) // エントリーを削除する
+		println("nums ++ List(\"iii\" -> 3, \"v\" -> 5): " + (nums ++ List("iii" -> 3, "v" -> 5)))	// 複数のエントリーを追加する
+		println("nums -- List(\"i\", \"li\", \"iii\"): " + (nums -- List("i", "li", "iii")))	// 複数のエントリーを削除する
+		println("nums.size: " + nums.size)	// Mapのサイズを返す
+		println("nums.contains(\"li\"): " + nums.contains("li"))	// キーが含まれているか確認する(return type: Boolean)
+		println("nums(\"li\"): " + nums("li"))	// 指定したキーに対応する値を返す
+		println("nums.keys: " + nums.keys)	// キー(Iterable)を返す
+		println("nums.keySet: " + nums.keySet)	// キーの集合を返す
+		println("nums.values: " + nums.values)	// 値(Itterable)を返す
+		println("nums.isEmpty: " + nums.isEmpty)	// 空かどうか確認する
+
+		val words = scala.collection.mutable.Map.empty[String, Int]
+		println("mutable.words: " + words)
+		words += ("one" -> 1)
+		println("add entry: " + words)
+		words -= "one"
+		println("delete entry: " + words)
+		words ++= List("one" -> 1, "two" -> 2, "three" -> 3)
+		println("add entrys: " + words)
+		words --= List("one", "two")
+		println("delete entrys: " + words)
 	}
 }
