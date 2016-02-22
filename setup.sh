eb6eeb50377f5a0748be9eafd7f73e3b857e9f4a
Yuki Yoshikawa <passion.efforts.freelance@gmail.com>
2016-02-22 23:59:34 +0900

    create Vagrantfile

---
+++ setup.sh
@@ -0,0 +1,16 @@
+# Install Java
+sudo yum -y install java-1.8.0-openjdk.x86_64
+sudo yum -y install java-1.8.0-openjdk-devel.x86_64
+
+# Install Scala
+wget http://downloads.typesafe.com/scala/2.11.7/scala-2.11.7.tgz
+tar xvzf scala-2.11.7.tgz
+sudo mv scala-2.11.7 /usr/local/scala
+
+# Install SBT
+curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
+sudo yum -y install sbt
+
+# Add Environment
+export PATH=$PATH:/usr/local/scala/bin
+echo $PATH
