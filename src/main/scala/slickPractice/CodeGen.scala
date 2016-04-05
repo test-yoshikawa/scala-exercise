2db44623f3410bf3b90364d2412a1879689ed731
Yuki Yoshikawa <passion.efforts.freelance@gmail.com>
2016-04-05 17:06:17 +0900

    add slick, typesafe config. implement slick example

---
+++ src/main/scala/slickPractice/CodeGen.scala
@@ -0,0 +1,20 @@
+package slickPractice
+
+import slick.codegen.SourceCodeGenerator
+
+/**
+ * テーブル定義を自動出力（Tables.scalaが作成される）
+ */
+object CodeGen extends App {
+  val slickDriver = "slick.driver.PostgresDriver"
+  val jdbcDrive = "org.postgresql.Driver"
+  val url = "jdbc:postgresql://localhost/archive"
+  val outputDir = "src/main/scala"
+  val pkg = "slickPractice"
+  val user = "postgres"
+  val password = "postgres"
+
+  SourceCodeGenerator.main(
+    Array(slickDriver, jdbcDrive, url, outputDir, pkg, user, password)
+  )
+}
