ad8488315f257bb13106f0a0349b58d6e49252e7
Yuki Yoshikawa <passion.efforts.freelance@gmail.com>
2016-04-24 15:45:27 +0900

    rename folder

---
+++ src/main/scala/basic/05_buildincontrol/MultiTable.scala
@@ -0,0 +1,37 @@
+package basic.buildincontrol
+
+/**
+ * 掛け算テーブル表示
+ */
+object MultiTable {
+	def main(args: Array[String]) = {
+		val table = multiTable()
+		table.foreach(print)
+		println()
+	}
+
+	/**
+	 * 1段分をシーケンスとして返す
+	 */
+	def makeRowSeq(row: Int) = {
+		for (col <- 1 to 10) yield {
+			val prod = (row * col).toString
+			val padding = " " * (4 - prod.length)
+			padding + prod
+		}
+	}
+
+	/**
+	 * 1段分を文字列として返す
+	 */
+	def makeRow(row: Int) = makeRowSeq(row).mkString
+
+	/**
+	 * 行に1段分の文字列を収めた表を返す
+	 */
+	def multiTable() = {
+		val tableSeq =
+			for (row <- 1 to 10) yield makeRow(row)
+		tableSeq.mkString("\n")
+	}
+}
---
+++ src/main/scala/basic/06_functionsandclosures/FunctionStyle.scala
@@ -0,0 +1,91 @@
+package basic.functionsandclosures
+
+/**
+ * 関数型スタイル　サンプル
+ */
+object FunctionalStyle {
+	def main(args: Array[String]) {
+		// 関数リテラルの例
+		println("==== Function literal example ====")
+		var increase = (x: Int) => {
+			println("increase 1!")
+			x + 1
+		}
+		println(increase(10))
+
+		// 関数リテラルの短縮形
+		println("\n==== Shortened form of function literal ====")
+		val someNumbers = List(-11, -10, -5, 0, 5, 10)
+		someNumbers.filter((x: Int) => x > 0)	// 通常
+		someNumbers.filter((x) => x > 0)		// パラメータの型は短縮できる
+		val filteredList1 = someNumbers.filter(x => x > 0)			// 括弧は省略可能
+		println(filteredList1)
+
+		// プレースホルダー構文
+		println("\n==== Placeholder ====")
+		val filteredList2 = someNumbers.filter(_ > 0)
+		println(filteredList2)
+		someNumbers.foreach(println _)
+		val f = (_: Int) + (_: Int)
+		println(f(10, 999))
+
+		// 部分適用された関数
+		println("\n==== Partial function ====")
+		val a = sum _
+		println(a(1, 2, 3))
+		val b = sum(1, _:Int, 3)
+		println(b(4))
+
+		// クロージャー
+		println("\n==== Closure ====")
+		var more = 4
+		// クロージャー（自由変数を含む関数オブジェクト。x:束縛変数, more:自由変数）
+		val addMore = (x: Int) => x + more
+		println(addMore(11))
+		more = 9
+		println(addMore(11)) // 自由変数moreを変えるとaddMoreメソッドの結果も変わる
+		var sum1 = 0
+		someNumbers.foreach(sum1 += _)	// クロージャー
+		println(sum1)
+		val inc1 = makeIncreaser(1)
+		val inc999 = makeIncreaser(999)
+		println(inc1(1))
+		println(inc999(1))
+
+		// 関数呼び出しの特殊な形態
+		// 連続パラメータ
+		println("\n==== Serial parameter ====")
+		echo()
+		echo("one")
+		echo("hello ", "world!")
+		val arr = Array("What's", "up", "doc?")
+		echo(arr: _*)	// 配列を渡す場合
+		println("==== Named arguments ====")
+		println(sum(c = 1, b = 2, a = 4))
+
+		println("\n==== Default parameter ====")
+		printTime()
+		printTime(out = Console.err)
+		printTime(divisor = 1000)
+
+	}
+
+	/**
+	 * 連続パラメータを利用したメソッド
+	 */
+	def echo(args: String*) = for(arg <- args) println(arg)
+
+	/**
+	 * 加算クロージャー
+	 */
+	def makeIncreaser(more: Int) = (x: Int) => x + more
+
+	/**
+	 * 引数の合計
+	 */
+	def sum(a: Int, b:Int, c:Int) = a + b + c
+
+	def printTime(out: java.io.PrintStream = Console.out, divisor: Int = 1) = {
+		out.println("time = " + System.currentTimeMillis() / divisor)
+	}
+}
